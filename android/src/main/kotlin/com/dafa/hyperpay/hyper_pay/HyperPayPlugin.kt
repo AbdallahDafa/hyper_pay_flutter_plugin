4942b5cfe3f4bdf0ce56919f9779936473d85b50
AbdallahDafa <abdallah.mahmoud@dafa.sa>
2025-04-28 12:47:04 +0300

    still in make class "HyperPayPlugin.kt" call instead of old code of MainActivity

--- android/src/main/kotlin/com/dafa/hyperpay/hyper_pay/HyperPayPlugin.kt
+++ android/src/main/kotlin/com/dafa/hyperpay/hyper_pay/HyperPayPlugin.kt
@@ -1,40 +1,43 @@
 package com.dafa.hyperpay.hyper_pay
 
-import io.flutter.Log
+//import io.flutter.Log
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.MethodCall
-import io.flutter.plugin.common.MethodChannel
+//import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 
+import android.Manifest
+import android.content.pm.PackageManager
+import androidx.annotation.NonNull
+import androidx.core.app.ActivityCompat
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.BinaryMessenger
+import io.flutter.plugin.common.MethodChannel
+import io.flutter.plugin.common.EventChannel
+
+///---------------------------------- hyperpay import files
+import com.dafa.hyperpay.hyper_pay.core.flutter.HyperpayFlutterChannelController
+import com.dafa.hyperpay.hyper_pay.core.flutter.HyperpayFlutterRequest
+import android.util.Log
+import com.dafa.hyperpay.hyper_pay.core.router.HyperPayRouter
+
 /** HyperPayPlugin */
-class HyperPayPlugin: FlutterPlugin, MethodCallHandler {
-  /// The MethodChannel that will the communication between Flutter and native Android
-  ///
-  /// This local reference serves to register the plugin with the Flutter Engine and unregister it
-  /// when the Flutter Engine is detached from the Activity
+class HyperPayPlugin: FlutterPlugin {
+
+  companion object   {
+    var eventSink: EventChannel.EventSink? = null
+  }
+
   private lateinit var channel : MethodChannel
 
   override fun onAttachedToEngine(flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
-    channel = MethodChannel(flutterPluginBinding.binaryMessenger, "hyper_pay")
+//    channel = MethodChannel(flutterPluginBinding.binaryMessenger, "hyper_pay")
     Log.i("abdo", "HyperPayPlugin - channel: $channel")
-    channel.setMethodCallHandler(this)
+//    channel.setMethodCallHandler(this)
   }
 
-  override fun onMethodCall(call: MethodCall, result: Result) {
-    Log.i("abdo", "HyperPayPlugin - onMethodCall() - result: $result")
-
-    if (call.method == "getPlatformVersion") {
-      Log.i("abdo", "HyperPayPlugin - onMethodCall() - getPlatformVersion - success")
-      result.success("Test Android ${android.os.Build.VERSION.RELEASE}")
-
-    }  else if ( call.method == "fromFlutter") {
-      Log.i("abdo", "HyperPayPlugin - onMethodCall() - method: fromFlutter - result: $result")
-
-    } else {
-      result.notImplemented()
-    }
-  }
 
   override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
     channel.setMethodCallHandler(null)
