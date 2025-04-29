8b34cb2f4eccf172c803d2e1146db249b4b14455
AbdallahDafa <abdallah.mahmoud@dafa.sa>
2025-04-29 17:03:13 +0300

    succress run simulator run swift code native

--- ios/Classes/HyperPayPlugin.swift
+++ ios/Classes/HyperPayPlugin.swift
@@ -18,48 +18,7 @@ public class HyperPayPlugin: NSObject, FlutterPlugin {
         result("iOS getPlatformVersion from swift native to flutter" ) //+ UIDevice.current.systemVersion
 
     default:
-//       result(FlutterMethodNotImplemented)
         result( "no implementation found")
     }
   }
 }
-
-//
-// public class HyperPayPlugin: NSObject, FlutterPlugin {
-//
-//
-//
-//   public static func register(with registrar: FlutterPluginRegistrar)    {
-//
-//
-//     print("abdo - HyperPayPlugin - register() swift code ");
-//   }
-//
-//
-// public func handlerWithMainActorThread(_ registrar: FlutterPluginRegistrar) async {
-// //    await MainActor.run {
-//             let channel = FlutterMethodChannel(name: "hyper_pay", binaryMessenger: registrar.messenger())
-//             let instance = HyperPayPlugin()
-// //             registrar.addMethodCallDelegate(instance, channel: channel)
-//             register.addMethodCallDelegate (_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-//
-//
-//             }
-// //           }
-// }
-//
-//
-//  @MainActor
-//   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult)   {
-//     if call.method == "getPlatformVersion" {
-//
-// //             DispatchQueue.main.async {
-// //
-// //             }
-//         result(UIDevice.current.name)
-//
-//     } else {
-//    //   result(FlutterMethodNotImplemented)
-//     }
-//   }
-// }
