824c59d940c110c2dfda854b51b278c3ee86b363
AbdallahDafa <abdallah.mahmoud@dafa.sa>
2025-05-05 17:18:22 +0300

    Feature  : Auto Detect Brand Type:
      * still native swift, success example by adding schema

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -137,7 +137,7 @@ class _MyAppState extends State<MyApp> {
     var channelRequest = HyperpayChannelRequest ( );
     channelRequest.shopperResultUrl =   "com.tuxedo.dafa.payment";  //contact hyperpay support to get merchantId
     channelRequest.merchantId =  "merchant.com.tuxedo.dafa";  //contact hyperpay support to get merchantId
-    channelRequest.checkoutId = "35D5DF7F78A78144EC6667CF3D047FC1.uat01-vm-tx01"; //get from your server side
+    channelRequest.checkoutId = "A89DACF183BB1056413C2F140CA34A66.uat01-vm-tx03"; //get from your server side
     channelRequest.amount =  1;
     channelRequest.isTest = true ; //false means it's live
 
